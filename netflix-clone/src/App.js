e18681ef4fdf7eb6551cedda4d5d3c674442c8d4
greatest-berhanuworie <berhanuworkie97@gmial.com>
2024-11-08 05:19:35 -0300

    second commit

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Berhanu Workie</h1>
+     <Home/>
     </div>
   );
 }
